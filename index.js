80528d138fe989746b67de35849c35bb35df8787
Christina Lambiase <christina.lambiase@gmail.com>
2023-06-17 12:39:07 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+function superbowlWin(record) {
+    const winningGame = record.find((game) => {
+        return game.result === "W"
+    })
+    if (winningGame) {
+        return winningGame.year
+    }
+
+}
